ffdb5350a6a2086d37f2f7af5225af0cafdcee5a
cuongvu3009 <cuongvu3009@gmail.com>
2022-04-05 11:53:16 +0300

    basic react-redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: 'INC' });
+  };
+
+  const decrement = () => {
+    dispatch({ type: 'DEC' });
+  };
+
+  const addBy10 = () => {
+    dispatch({ type: 'ADD', payload: 10 });
+  };
+
+  const minus10 = () => {
+    dispatch({ type: 'MINUS', payload: 10 });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Counter</h2>
+      <p>{counter}</p>
+      <button onClick={() => increment()}>+</button>
+      <button onClick={() => decrement()}>-</button>
+      <button onClick={() => addBy10()}>Add 10</button>
+      <button onClick={() => minus10()}>Minus 10</button>
     </div>
   );
 }
